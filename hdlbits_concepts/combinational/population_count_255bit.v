562bc1d004327a6332a10d4c24f969618bf04f07
Sireesha-P-hdl <sireesha.p.ec@gmail.com>
2026-01-22 17:24:00 +0530

    Update population_count_255bit.v

--- hdlbits_concepts/combinational/population_count_255bit.v
+++ hdlbits_concepts/combinational/population_count_255bit.v
@@ -3,14 +3,7 @@
 // Description: Counts the number of '1' bits in a 255-bit input vector.
 // The output represents the population count of the input.
 //------------------------------------------------------------------------------
-//final carry = carry[99]
-//cout[0], cout[1], ..., cout[99]
-// For bit 0:
-//carry[0] comes from cin
-//For bit 1:
-//carry[1] comes from carry[0]
-//For bit i:
-//carry[i] depends on carry[i−1]
+
 module population_count_255bit (
     input  [254:0] in,
     output reg [7:0] out
