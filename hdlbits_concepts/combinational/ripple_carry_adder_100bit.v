0e1dc0bdd45e7d1da4e34cd5cdd8f3817cc03303
Sireesha-P-hdl <sireesha.p.ec@gmail.com>
2026-01-22 17:24:34 +0530

    Update ripple_carry_adder_100bit.v

--- hdlbits_concepts/combinational/ripple_carry_adder_100bit.v
+++ hdlbits_concepts/combinational/ripple_carry_adder_100bit.v
@@ -3,6 +3,14 @@
 // Description: Implements a 100-bit ripple carry adder.
 // Bit 0 uses an external carry-in, while higher bits propagate the carry.
 //------------------------------------------------------------------------------
+//final carry = carry[99]
+//cout[0], cout[1], ..., cout[99]
+// For bit 0:
+//carry[0] comes from cin
+//For bit 1:
+//carry[1] comes from carry[0]
+//For bit i:
+//carry[i] depends on carry[i−1]
 module ripple_carry_adder_100bit (
     input  [99:0] a,
     input  [99:0] b,
