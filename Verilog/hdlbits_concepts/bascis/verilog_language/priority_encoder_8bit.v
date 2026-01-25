60d0c48a9740b12fe780ffd0f3f5fe644fcf9563
Sireesha-P-hdl <sireesha.p.ec@gmail.com>
2026-01-25 17:50:41 +0530

    Create priority_encoder_8bit.v

---
+++ Verilog/hdlbits_concepts/bascis/verilog_language/priority_encoder_8bit.v
@@ -0,0 +1,26 @@
+//------------------------------------------------------------------------------
+// Module: priority_encoder_8bit
+// Description: Encodes the position of the least significant '1' bit
+// in an 8-bit input vector using casez.
+//------------------------------------------------------------------------------
+module priority_encoder_8bit (
+    input  [7:0] in,
+    output reg [2:0] pos
+);
+
+    always @(*) begin
+        pos = 3'd0;
+        casez (in)
+            8'bzzzzzzz1: pos = 3'd0;
+            8'bzzzzzz1z: pos = 3'd1;
+            8'bzzzzz1zz: pos = 3'd2;
+            8'bzzzz1zzz: pos = 3'd3;
+            8'bzzz1zzzz: pos = 3'd4;
+            8'bzz1zzzzz: pos = 3'd5;
+            8'bz1zzzzzz: pos = 3'd6;
+            8'b1zzzzzzz: pos = 3'd7;
+            default:     pos = 3'd0;
+        endcase
+    end
+
+endmodule
