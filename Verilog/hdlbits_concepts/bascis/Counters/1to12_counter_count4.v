6af85705e26b656391430a34ab13c823a8a663f1
Sireesha-P-hdl <sireesha.p.ec@gmail.com>
2026-02-07 14:45:44 +0530

    Update 1to12_counter_count4.v

--- Verilog/hdlbits_concepts/bascis/Counters/1to12_counter_count4.v
+++ Verilog/hdlbits_concepts/bascis/Counters/1to12_counter_count4.v
@@ -10,9 +10,9 @@
 // - Uses provided count4 module (enable/load with load priority)
 //
 // Control Logic:
-// c_enable  = enable (pass through)
-// c_load    = reset | (Q==12 & enable)  (load when reset OR rollover)
-// c_d       = 4'd1 (always load 1)
+// c_enable  = enable (pass through)   If external enable is high → counter can count ,If low → counter holds ,✔ Direct connection
+// c_load    = reset | (Q==12 & enable)  (load when reset OR rollover)Load happens whenreset = 1OR Q == 12 AND enable = 1 ,This ensures:Reset forces counter to 1 ,After reaching 12 → next cycle loads 1
+// c_d       = 4'd1 (always load 1) Whenever load happens, counter loads 1
 // =================================================================
 
 module top_module (
@@ -32,7 +32,7 @@ module top_module (
     assign c_load = reset | (Q == 4'd12 & enable);
     
     // Always load value = 1 (0001 binary)
-    assign c_d = 4'd1;
+    assign c_d =  (c_load)? 1:0;
     
     // Instantiate provided 4-bit counter
      count4 the_counter (clk, c_enable, c_load, c_d, Q);
